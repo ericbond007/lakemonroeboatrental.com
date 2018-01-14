6775877d5034584e75b8fd8636e196b0dc88500e
Eric Bond <ericbond007@gmail.com>
2018-01-14 11:02:07 -0500

    working on deployment process

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import Home from '../../components/Home/Home';
+/*import Home from './Home';*/
 
 class HomePage extends Component {
   componentDidMount() {
