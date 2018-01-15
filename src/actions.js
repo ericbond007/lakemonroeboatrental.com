6dae450df486ef66002644cadb603cccd99bdfe6
Eric Bond <ericbond007@gmail.com>
2018-01-15 11:12:00 -0500

    still working on adding redux

---
+++ src/actions.js
@@ -0,0 +1,7 @@
+export const GET_BOATS = 'GET_BOATS'
+export const GET_BOAT_BY_ID = 'GET_BOAT_BY_ID'
+export const GET_RATES = 'GET_RATES'
+export const GET_POLICY = 'GET_POLICY'
+export const GET_CONTACT = 'GET_CONTACT'
+export const GET_HOURS = 'GET_HOURS'
+
