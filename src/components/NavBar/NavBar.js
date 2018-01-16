fec190cb2a24a89767e262c722a8369ac9073c8d
Eric Bond <ericbond007@gmail.com>
2018-01-16 14:04:47 -0500

    updated hover/active nav links

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Tabs } from 'reactbulma'
 
 import './NavBar.css';
 
@@ -9,15 +8,15 @@ class NavBar extends Component {
 
   render() {
     return (
-      <Tabs centered fullwidth  medium id="noscroll">
+      <div className="tabs is-centered is-fullwidth is-medium" id="noscroll">
         <ul id="navBarbig">
-          <li><Link to="/" >LMBR</Link></li>
+          <li><Link activeClassName="active" to="/" >LMBR</Link></li>
             <li ><Link to="/boats">Boats</Link></li>
             <li ><Link to="/rates">Rates</Link></li>
             <li ><Link to="/policies">Policies</Link></li>
             <li ><Link to="/contact" >Contact</Link></li>
           </ul>
-        </Tabs>
+        </div>
     );
   }
 }
