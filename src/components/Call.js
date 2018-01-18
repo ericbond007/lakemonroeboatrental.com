ca61f0329633ef8fda7da1592a769971d163150a
Eric Bond <ericbond007@gmail.com>
2018-01-18 17:22:41 -0500

    added <Call> component for phone #

---
+++ src/components/Call.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Call = ({className}) => (
+  <div className={className}>
+    <a href="tel:+1-812-837-9909">812-837-9909</a>
+</div>
+);
+
+export default Call;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,7 @@ import SEO from '../SEO';
 import Boats from './Boats';
 import { Section, Container, Title, SubTitle } from 'reactbulma';
 import Image from '../Image';
+import Call from '../Call';
 
 import './Home.css';
 
@@ -28,7 +29,7 @@ class Home extends Component {
             <h1 className="title is-1 is-spaced">Lake Monroe Boat Rental</h1>
             <h2 className="subtitle is-3"> Call to Reserve Today!</h2>
             <h3>
-                812-837-9474
+              <Call />
               </h3>
               <h4>
                 Located just south east of Bloomingtion in Paynetown State Recreational Area
