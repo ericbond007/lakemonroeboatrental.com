3a466f2c17372af62df9eae4251c69c3df850a4a
Eric Bond <ericbond007@gmail.com>
2018-01-18 15:41:05 -0500

    working on navbar styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -23,7 +23,7 @@ class NavBar extends Component {
                   <ul id="navBarbig">
                       <li ><NavLink activeClassName="active" to="/rates">Rates</NavLink></li>
                       <li ><NavLink activeClassName="active" to="/boats">Boats</NavLink></li>
-                    <li><NavLink  exact activeClassName="active" to="/" >LMBR</NavLink></li>
+                      <li><NavLink  exact activeClassName="active" to="/" >LMBR</NavLink></li>
                       <li ><NavLink activeClassName="active" to="/contact" >Contact</NavLink></li>
                       <li ><NavLink activeClassName="active" to="/policies">Policies</NavLink></li>
                     </ul>
