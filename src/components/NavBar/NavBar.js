82124b5f34136d30eeefc65b18b81923e4fc1090
Eric Bond <ericbond007@gmail.com>
2018-01-24 11:48:49 -0500

    fixed overflow on navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,7 +5,7 @@ import './NavBar.css';
 
 const NavBar = () => (
                 <div className="tabs is-centered is-fullwidth is-medium">
-                  <div className="navTabs">
+                  <div className="navTabs" id="noscroll">
                   <ul  id="navBarbig">
                       <li ><NavLink activeClassName="active" to="/rates">Rates</NavLink></li>
                       <li ><NavLink activeClassName="active" to="/boats">Boats</NavLink></li>
