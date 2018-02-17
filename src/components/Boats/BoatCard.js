deb5dde764890f6cba55a8114a168691f88cfe16
Eric Bond <ericbond007@gmail.com>
2018-02-16 22:50:24 -0500

    working on the boatcard

--- src/components/Boats/BoatCard.js
+++ src/components/Boats/BoatCard.js
@@ -7,12 +7,14 @@ import { HashLink as Link } from 'react-router-hash-link';
 
 const BoatCard = ({ cardImg, cardHeader, cardText, cardLink, imgAlt }) => {
   return (
-      <Slide bottom>
-    <Scroll type="id" element="herobottom">
     <div className="boatCard ripple">
+      <Slide bottom>
+        <Scroll type="id" element="herobottom" style={{boxShadow: '5px 8px 15px -5px rgba(66,65,69,1)', borderRadius: '10px'}}>
+      <div style={{borderRadius: '10px', boxShadow: '5px 8px 15px -5px rgba(66,65,69,1)'}}>
     <Link to={{
       pathname: `/boats/${cardLink}`,
     }}>
+    <div className="">
     <div className="card">
       <div className="card-header">
         <p className="card-header-title is-size-3-touch">
@@ -21,19 +23,21 @@ const BoatCard = ({ cardImg, cardHeader, cardText, cardLink, imgAlt }) => {
       </div>
       <div className="card-image">
         <figure className="is-4by3 image">
-          <Image imgName={cardImg} imgAlt={imgAlt} imgLink={cardLink} style={{width: '100%'}}/>
+          <Image imgName={cardImg} imgAlt={imgAlt} imgLink={cardLink}  divClassName="imgDrop" style={{width: '100%'}}/>
         </figure>
       </div>
       <div className="card-content">
         <div className="content is-size-4-touch">
-          <p>{cardText}</p>
+          <p >{cardText}</p>
         </div>
       </div>
     </div>
+</div>
   </Link>
 </div>
   </Scroll>
 </Slide>
+</div>
   );
   }
 
