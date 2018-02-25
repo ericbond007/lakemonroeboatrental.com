6269a1be8572f84588064fe2329293a35e5123f7
Eric Bond <ericbond007@gmail.com>
2018-02-25 16:54:36 -0500

    updated ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,21 +7,18 @@ module.exports = {
 
     // First application
     {
-      name      : 'API',
-      script    : 'app.js',
+      name      : 'lmbr',
+      script    : 'server/index.js',
       env: {
         COMMON_VARIABLE: 'true'
       },
       env_production : {
-        NODE_ENV: 'production'
+        NODE_ENV: 'production',
+        PORT: '3000',
       }
     },
 
     // Second application
-    {
-      name      : 'WEB',
-      script    : 'web.js'
-    }
   ],
 
   /**
@@ -30,12 +27,15 @@ module.exports = {
    */
   deploy : {
     production : {
-      user : 'node',
-      host : '212.83.163.1',
+      user : 'ubuntu',
+      key  : '/home/eric/ebondawskey.pem',
+      host : 'ec2-54-89-228-69.compute-1.amazonaws.com',
       ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      repo : 'git@github.com:ericbond007/lakemonroeboatrental.com.git',
+      path : '/home/ubuntu/apps/lmbr.com',
+      'ssh_options': 'ForwardAgent=yes',
+      'pre-deploy': 'yarn build',
+      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env production'
     },
     dev : {
       user : 'node',
